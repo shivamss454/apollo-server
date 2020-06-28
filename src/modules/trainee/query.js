3668e89ea7f7c77ef357ef364032b9f3fc81a0c2
shivamss454 <shivam.sharma@successive.tech>
2020-06-28 20:42:25 +0530

    fix: change in feature/58635

--- src/modules/trainee/query.js
+++ src/modules/trainee/query.js
@@ -7,7 +7,7 @@ export default {
       const { data: { skip, limit } } = args;
       const { dataSources: { traineeAPI } } = context;
       const res = await traineeAPI.getTrainee({ skip, limit });
-      return res.data.records;
+      return res.data;
     } catch (error) {
       return new UserInputError('invalid Arguments', {
         invalidArgs: Object.keys(args),
